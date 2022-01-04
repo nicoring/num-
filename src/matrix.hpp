4e7c29cb985ae4a93d641aab9fca091c5e677499
Nico Ring <ringnico@web.de>
2022-01-04 12:33:15 +0100

    operators && corrected dot product && random generation

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,56 +1,61 @@
 #include <cstddef>
 #include <iostream>
+#include <random>
 #include <vector>
 
+namespace mat {
+
 struct shape_t {
-    size_t rows, cols;
+    uint32_t rows;
+    uint32_t cols;
 };
 
-template <class T> class mat {
+template <typename T> class matrix {
+  private:
     std::vector<T> data;
-    const size_t rows;
-    const size_t cols;
+    const uint32_t rows;
+    const uint32_t cols;
 
   public:
-    mat() : rows(0), cols(0){};
-    ~mat(){};
-    mat(size_t rows, size_t cols) : rows(rows), cols(cols) {
-        data = std::vector<T>(rows * cols);
-    }
-    mat(size_t rows, size_t cols, T fill_value) : rows(rows), cols(cols) {
-        data = std::vector<T>(rows * cols, fill_value);
-    }
+    matrix() : matrix(0, 0){};
 
-    shape_t shape() { return shape_t{rows, cols}; };
-    bool empty() { return rows == 0 and cols == 0; };
+    matrix(uint32_t rows, uint32_t cols)
+        : rows(rows), cols(cols), data(rows * cols){};
 
-    inline T get(size_t row, size_t col) const {
+    matrix(uint32_t rows, uint32_t cols, T fill_value)
+        : rows(rows), cols(cols), data(rows * cols, fill_value){};
+
+    shape_t shape() const { return shape_t{rows, cols}; };
+
+    bool empty() const { return rows == 0 && cols == 0; };
+
+    inline T get(uint32_t row, uint32_t col) const {
         return data[rows * row + col];
-    }
+    };
 
-    inline void set(size_t row, size_t col, T value) {
+    inline void set(uint32_t row, uint32_t col, T value) {
         data[rows * row + col] = value;
-    }
+    };
 
     void print() const {
-        for (size_t row = 0; row < rows; row++) {
-            for (size_t col = 0; col < cols; col++) {
+        for (uint32_t row = 0; row < rows; row++) {
+            for (uint32_t col = 0; col < cols; col++) {
                 std::cout << get(row, col) << " ";
             }
             std::cout << std::endl;
         }
-    }
+    };
 
-    mat dot(mat other) const {
-        if (not(cols == other.rows)) {
+    matrix dot(matrix other) const {
+        if (cols != other.rows) {
             throw std::invalid_argument(
                 "inner dimensions don't match for dot product");
         }
-        mat new_mat = mat(rows, other.cols);
-        for (size_t new_row = 0; new_row < rows; new_row++) {
-            for (size_t new_col = 0; new_col < other.cols; new_col++) {
+        matrix new_mat(rows, other.cols);
+        for (uint32_t new_row = 0; new_row < rows; new_row++) {
+            for (uint32_t new_col = 0; new_col < other.cols; new_col++) {
                 T value = 0;
-                for (size_t inner = 0; inner < cols; inner++) {
+                for (uint32_t inner = 0; inner < cols; inner++) {
                     value += get(new_row, inner) * other.get(inner, new_col);
                 }
                 new_mat.set(new_row, new_col, value);
@@ -59,35 +64,110 @@ template <class T> class mat {
         return new_mat;
     };
 
-    mat transpose() const {
-        mat new_mat = mat(cols, rows);
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
+    matrix transpose() const {
+        matrix new_mat(cols, rows);
+        for (uint32_t row = 0; row < rows; row++) {
+            for (uint32_t col = 0; col < cols; col++) {
                 new_mat.set(col, row, get(row, col));
             }
         }
         return new_mat;
     };
 
-    mat operator+(T to_add) const {
-        mat new_mat = mat(cols, rows);
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                new_mat.set(row, col, get(row, col) + to_add);
-            }
-        }
-        return new_mat;
-    }
+#define add_operators(op)                                                      \
+    matrix operator op(T rhs) const {                                          \
+        matrix new_mat(cols, rows);                                            \
+        for (uint32_t row = 0; row < rows; row++) {                            \
+            for (uint32_t col = 0; col < cols; col++) {                        \
+                new_mat.set(row, col, get(row, col) op rhs);                   \
+            }                                                                  \
+        }                                                                      \
+        return new_mat;                                                        \
+    };                                                                         \
+                                                                               \
+    friend matrix operator op(T lhs, matrix& rhs) {                            \
+        matrix<T> new_mat(rhs.cols, rhs.rows);                                 \
+        for (uint32_t row = 0; row < rhs.rows; row++) {                        \
+            for (uint32_t col = 0; col < rhs.cols; col++) {                    \
+                new_mat.set(row, col, lhs op rhs.get(row, col));               \
+            }                                                                  \
+        }                                                                      \
+        return new_mat;                                                        \
+    };                                                                         \
+                                                                               \
+    matrix operator op(matrix& rhs) const {                                    \
+        matrix new_mat(cols, rows);                                            \
+        for (uint32_t row = 0; row < rows; row++) {                            \
+            for (uint32_t col = 0; col < cols; col++) {                        \
+                new_mat.set(row, col, get(row, col) op rhs.get(row, col));     \
+            }                                                                  \
+        }                                                                      \
+        return new_mat;                                                        \
+    };
 
-    mat operator+(mat other) const {
-        mat new_mat = mat(cols, rows);
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                new_mat.set(row, col, get(row, col) + other.get(row, col));
-            }
-        }
-        return new_mat;
-    }
-
-    friend mat operator+(T to_add, mat m) { return m + to_add; }
+    add_operators(+);
+    add_operators(-);
+    add_operators(*);
+    add_operators(/);
+#undef add_operators
 };
+
+template <typename T> matrix<T> zeros(uint32_t rows, uint32_t cols) {
+    return matrix<T>(rows, cols, 0);
+}
+
+template <typename T> matrix<T> ones(uint32_t rows, uint32_t cols) {
+    return matrix<T>(rows, cols, 1);
+}
+
+template <typename T> matrix<T> eye(uint32_t rows, uint32_t cols) {
+    matrix<T> mat(rows, cols, 0);
+    uint32_t min_count = std::min(rows, cols);
+    for (uint32_t i = 0; i < min_count; i++) {
+        mat.set(i, i, 1);
+    }
+    return mat;
+}
+
+template <typename T> matrix<T> eye(uint32_t size) {
+    return eye<T>(size, size);
+}
+
+matrix<double> rand(uint32_t rows, uint32_t cols) {
+    matrix<double> mat(rows, cols);
+
+    // Will be used to obtain a seed for the random number engine
+    std::random_device rd;
+    // Standard mersenne_twister_engine seeded with rd()
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> uniform(0.0, 1.0);
+
+    for (uint32_t row = 0; row < rows; row++) {
+        for (uint32_t col = 0; col < cols; col++) {
+            mat.set(row, col, uniform(gen));
+        }
+    }
+
+    return mat;
+}
+
+matrix<double> randn(uint32_t rows, uint32_t cols, double mean = 0,
+                     double stddev = 1) {
+    matrix<double> mat(rows, cols);
+
+    // Will be used to obtain a seed for the random number engine
+    std::random_device rd;
+    // Standard mersenne_twister_engine seeded with rd()
+    std::mt19937 gen(rd());
+    std::normal_distribution<> normal(mean, stddev);
+
+    for (uint32_t row = 0; row < rows; row++) {
+        for (uint32_t col = 0; col < cols; col++) {
+            mat.set(row, col, normal(gen));
+        }
+    }
+
+    return mat;
+}
+
+} // namespace mat
--- src/run.cpp
+++ src/run.cpp
@@ -3,10 +3,10 @@
 #include "matrix.hpp"
 
 int main() {
-    mat<double> empty_matrix;
+    mat::matrix<double> empty_matrix;
     assert(empty_matrix.empty());
-    mat<double> matrix(2, 2, 0);
-    mat<double> id(2, 2, 0);
+    mat::matrix<double> matrix(2, 2, 0);
+    mat::matrix<double> id(2, 2, 0);
     id.set(0, 0, 1);
     id.set(1, 1, 1);
     matrix.set(0, 0, 1);
@@ -21,5 +21,8 @@ int main() {
 
     (1 + id + 1).print();
     (id + id).print();
+    (id - 10).print();
+    (id / 2).print();
+    (id * 100).print();
     return 0;
 }
